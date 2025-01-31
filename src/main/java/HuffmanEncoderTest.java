b1647128f4687cc0e25c79057a09847764dfcf8a
Chris <cholla89@gmail.com>
2025-01-31 14:01:17 -0500

    Add HuffmanTree, HuffmanEncoder, Main, and HuffmanTest classes

---
+++ src/main/java/HuffmanEncoderTest.java
@@ -0,0 +1,8 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Map;
+
+public class HuffmanEncoderTest {
+
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Map;
+
+/**
+ * Main method for the Huffman Encoder
+ * @author chrisholland
+ * @version 2024.12
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        if(args.length != 1) {
+            System.out.println("Invalid arguments. Please provide the input file path.");
+            System.exit(1);
+
+        }
+
+    }
+}
